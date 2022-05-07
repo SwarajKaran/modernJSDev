205c6d1436ee1e9ad114cfb4ef94118d42cc827f
swarajkaran <swarajkaran.raj@gmail.com>
2022-05-07 23:50:40 +0530

    default import

--- script.js
+++ script.js
@@ -3,11 +3,14 @@
 // addtoCart('bread', 5);
 // console.log(price, tq);
 
-import * as ShoppingCart from './shoppingCart.js';
+// import * as ShoppingCart from './shoppingCart.js';
 
 //for named imports, use curly braces and same name
 console.log('Importing Module');
 // console.log(shippingCost); // it is a part of shoppingCart.js. It is not a global variable
 // to use this we need tio export this variable in shoppingCart.js
-ShoppingCart.addtoCart('bread', 5);
-console.log(ShoppingCart.totalPrice);
+// ShoppingCart.addtoCart('bread', 5);
+// console.log(ShoppingCart.totalPrice);
+// import add, { addtoCart, totalPrice as price, tq } from './shoppingCart.js'; // we can mix default and named but not advisable
+import add from './shoppingCart.js';
+add('butter', 250);
--- shoppingCart.js
+++ shoppingCart.js
@@ -12,3 +12,9 @@ const totalPrice = 237;
 const totalQuantity = 23;
 
 export { totalPrice, totalQuantity as tq };
+
+// when we want to export only one thing
+export default function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(`${quantity} ${product} added to cart`);
+}
