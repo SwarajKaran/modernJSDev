c83a3b85964d3862d3cbcd19656809eb3d15668e
swarajkaran <swarajkaran.raj@gmail.com>
2022-05-07 23:37:18 +0530

    named export/import

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 // Importing Module
-import './shoppingCart.js';
+import { addtoCart, totalPrice, totalQuantity } from './shoppingCart.js';
+//for named imports, use curly braces and same name
 console.log('Importing Module');
+// console.log(shippingCost); // it is a part of shoppingCart.js. It is not a global variable
+// to use this we need tio export this variable in shoppingCart.js
+addtoCart('bread', 5);
+console.log(totalPrice, totalQuantity);
--- shoppingCart.js
+++ shoppingCart.js
@@ -1,2 +1,14 @@
 // Exporting Module
 console.log('Exporting Module');
+const shippingCost = 10;
+const cart = [];
+
+export const addtoCart = function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(`${quantity} ${product} added to cart`);
+}; // named export
+
+const totalPrice = 237;
+const totalQuantity = 23;
+
+export { totalPrice, totalQuantity };
