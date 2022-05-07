9d13c7b30b5b2de0d9f7e6cfe02d17db7a412444
swarajkaran <swarajkaran.raj@gmail.com>
2022-05-07 23:55:51 +0530

    import are live connection not copy  of export

--- script.js
+++ script.js
@@ -7,10 +7,14 @@
 
 //for named imports, use curly braces and same name
 console.log('Importing Module');
-// console.log(shippingCost); // it is a part of shoppingCart.js. It is not a global variable
+// console.log(shippingCost); // it is a part of shoppingCart.js. It is not a global variable { addtoCart, totalPrice as price, tq }
 // to use this we need tio export this variable in shoppingCart.js
 // ShoppingCart.addtoCart('bread', 5);
 // console.log(ShoppingCart.totalPrice);
 // import add, { addtoCart, totalPrice as price, tq } from './shoppingCart.js'; // we can mix default and named but not advisable
-import add from './shoppingCart.js';
+import add, { cart } from './shoppingCart.js';
 add('butter', 250);
+
+add('bread', 5);
+add('apple', 2);
+console.log(cart); // import are live connection, it is not copy of export
--- shoppingCart.js
+++ shoppingCart.js
@@ -1,7 +1,7 @@
 // Exporting Module
 console.log('Exporting Module');
 const shippingCost = 10;
-const cart = [];
+export const cart = [];
 
 export const addtoCart = function (product, quantity) {
   cart.push({ product, quantity });
